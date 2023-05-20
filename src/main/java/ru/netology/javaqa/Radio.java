d7b6bfd3e884de58999890a36d0e3a8a2d7938a4
Zaure <Zsaigutina@gmail.com>
2023-05-20 09:46:07 +0300

    src changed

--- src/main/java/ru/netology/javaqa/Radio.java
+++ src/main/java/ru/netology/javaqa/Radio.java
@@ -1,5 +1,12 @@
 package ru.netology.javaqa;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
     private int currentStation;
     private int maxStation = 9;
@@ -8,96 +15,4 @@ public class Radio {
     private int maxVolume = 100;
     private int minVolume = 0;
     private int stationQuantity = 10;
-
-    public Radio(int stationQuantity) {
-        this.stationQuantity = stationQuantity;
-        this.maxStation = stationQuantity - 1;
-        this.minStation = 0;
-    }
-
-    public Radio() {
-
-    }
-
-    public int getMaxStation () {
-        return maxStation;
-    }
-
-    public int getMinStation () {
-        return minStation;
-    }
-
-    public void next() {
-        if (currentStation != maxStation) {
-            currentStation++;
-        } else {
-            currentStation = minStation;
-        }
-    }
-
-    public void previous() {
-        if (currentStation != minStation) {
-            currentStation--;
-        } else {
-            currentStation = maxStation;
-        }
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation < minStation) {
-            return;
-        }
-        if (currentStation > maxStation) {
-            return;
-        }
-        this.currentStation = currentStation;
-    }
-
-    public void upVolume() {
-        if (currentVolume != maxVolume) {
-            currentVolume++;
-        } else {
-            currentVolume = maxVolume;
-        }
-    }
-
-    public void downVolume() {
-        if (currentVolume > minVolume) {
-            currentVolume = currentVolume - 1;
-        } else {
-            currentVolume = minVolume;
-        }
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < minVolume) {
-            return;
-        }
-        if (currentVolume > maxVolume) {
-            return;
-        }
-        this.currentVolume = currentVolume;
-    }
-    public int getMaxVolume () {
-        return maxVolume;
-    }
-
-    public int getMinVolume () {
-        return minVolume;
-    }
-
-    public int getStationQuantity () {
-        return stationQuantity;
-    }
 }
-
-
-
